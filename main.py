7c7b8e707b256c03c0b1e11cba385f4440a7e105
shireenchand <shireen.chand@yahoo.com>
2020-12-16 13:43:31 +0530

    made it better

--- main.py
+++ main.py
@@ -5,30 +5,68 @@ root.title("CALCULATOR")
 
 #CREATING THE INPUT AND DISPLAY FIELD
 e = Entry(root, width=35, borderwidth=5)
-e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+e.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 
 #FUNCTION FOR THE ON CLICK ACTION
 def buttonClick(x):
-        if(x=="  "):
-            e.delete(0, END)
-        else:
             current = e.get()
             e.delete(0, END)
             e.insert(0, str(current)+str(x))
-            eqn = e.get()
-            if(x==" "):
-                add(eqn)
+
+#FUNCTION TO CLEAR
+def buttonClear():
+    e.delete(0,END)
+
 
 #FUNCTION TO ADD THE NUMBERS
-def add(numbers):
-    num = numbers.split('+')
-    total = 0
-    for x in num:
-        sum = total + int(x)
-        total = sum
-    e.delete(0, END)
-    e.insert(0, sum)
+def add():
+    first_number = e.get()
+    global f_num
+    f_num = int(first_number)
+    global op
+    op = "+"
+    e.delete(0,END)
 
+#FUNCTION TO SUBTRACT THE NUMBERS
+def sub():
+    first_number = e.get()
+    global f_num
+    f_num = int(first_number)
+    global op
+    op = "-"
+    e.delete(0, END)
+
+#FUNCTION TO MULTIPLY THE NUMBERS
+def mul():
+    first_number = e.get()
+    global f_num
+    f_num = int(first_number)
+    global op
+    op = "x"
+    e.delete(0, END)
+
+#FUNCTION TO DIVIDE THE NUMBERS
+def div():
+    first_number = e.get()
+    global f_num
+    f_num = int(first_number)
+    global op
+    op = "/"
+    e.delete(0, END)
+
+#FUNCTION TO GET THE RESULT
+def equal():
+    second_number = e.get()
+    e.delete(0,END)
+    s_num = int(second_number)
+    if(op=='+'):
+        e.insert(0, f_num+s_num)
+    elif(op=='-'):
+        e.insert(0, f_num-s_num)
+    elif(op=='x'):
+        e.insert(0,f_num*s_num)
+    else:
+        e.insert(0,f_num/s_num)
 
 #CREATING THE BUTTONS
 button1 = Button(root,text="1", padx=40,pady=20,command=lambda: buttonClick(1))
@@ -41,9 +79,12 @@ button7 = Button(root,text="7", padx=40,pady=20,command=lambda: buttonClick(7))
 button8 = Button(root,text="8", padx=40,pady=20,command=lambda: buttonClick(8))
 button9 = Button(root,text="9", padx=40,pady=20,command=lambda: buttonClick(9))
 button0 = Button(root,text="0", padx=40,pady=20,command=lambda: buttonClick(0))
-buttonadd = Button(root,text="+", padx=40,pady=20,command=lambda: buttonClick("+"))
-buttonequal = Button(root,text="=", padx=40,pady=20,command=lambda: buttonClick(" "))
-buttonclear = Button(root, text="CLEAR",padx=40,pady=20,command=lambda: buttonClick("  ") )
+buttonadd = Button(root,text="+", padx=40,pady=20,command=add)
+buttonsub = Button(root,text="-", padx=40,pady=20,command=sub)
+buttonmul = Button(root,text="x", padx=40,pady=20,command=mul)
+buttondiv = Button(root,text="/", padx=40,pady=20,command=div)
+buttonequal = Button(root,text="=", padx=40,pady=20,command=equal)
+buttonclear = Button(root, text="CLEAR",padx=40,pady=20,command=buttonClear )
 
 #POSITIONING THE BUTTONS
 button1.grid(row=3,column=0, sticky="nsew")
@@ -56,9 +97,12 @@ button7.grid(row=1,column=0, sticky="nsew")
 button8.grid(row=1,column=1, sticky="nsew")
 button9.grid(row=1,column=2, sticky="nsew")
 button0.grid(row=4,column=0, sticky="nsew")
-buttonadd.grid(row=4,column=1, sticky="nsew")
-buttonequal.grid(row=4,column=2, sticky="nsew")
-buttonclear.grid(row=5, columnspan=3, sticky="nsew")
+buttonadd.grid(row=3,column=3, sticky="nsew")
+buttonsub.grid(row=2,column=3, sticky="nsew")
+buttonmul.grid(row=1,column=3, sticky="nsew")
+buttondiv.grid(row=4,column=3, sticky="nsew")
+buttonequal.grid(row=4,column=1, sticky="nsew")
+buttonclear.grid(row=4, column=2, sticky="nsew")
 
 
 #LOOP TO KEEP THE PROGRAM GOING ON
